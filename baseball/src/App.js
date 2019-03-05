7c3f07895b0ccbe9273ca09f6e585979948db888
spinon187 <spinon187@gmail.com>
2019-03-05 15:12:04 -0600

    most basic MVP functions

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,25 +1,113 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    state = {
+      balls: 0,
+      strikes: 0,
+      outs: 0,
+      atBat: 'away',
+      inning: 1
+    }
+    
+    ball = () => {
+      if(this.state.balls < 3){
+        this.setState({
+          ...this.state,
+          balls: this.state.balls + 1
+        })
+      }
+      else {
+        this.setState({
+          ...this.state,
+          balls: 0
+        })
+      }
+    }
+
+    strike = () => {
+      if(this.state.strikes < 2){
+        this.setState({
+          ...this.state,
+          strikes: this.state.strikes + 1
+        })
+      }
+      else {
+        this.setState({
+          ...this.state,
+          strikes: 0
+        })
+      }
+    }
+
+    out = () => {
+      if(this.state.outs < 2){
+        this.setState({
+          ...this.state,
+          strikes: this.state.outs + 1
+        })
+      }
+      else {
+        this.setState({
+          ...this.state,
+          outs: 0
+        })
+        if(this.state.atBat === 'away'){
+          this.setState({
+            ...this.state,
+            atBat: 'home'
+          })
+        }
+        else {
+          this.setState({
+            ...this.state,
+            atBat: 'away',
+            inning: this.state.inning + 1
+          })
+        }
+      }
+    }
+
+    hit = () => {
+      this.setState({
+        ...this.state,
+        balls: 0,
+        strikes: 0
+      })
+    }
+
+    foul = () => {
+      if(this.state.strikes < 2){
+        this.setState({
+          ...this.state,
+          strikes: this.state.strikes + 1
+        })
+      }
+    }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <div className='scoreboard'>
+       </div>
+       <div className='atBat'>
+       <div className='balls'>
+          <h2>Balls: {this.state.balls}</h2>
+        </div>
+        <div className='strikes'>
+          <h2>Strikes: {this.state.strikes}</h2>
+        </div>
+        <div className='outs'>
+          <h2>Outs: {this.state.outs}</h2>
+        </div>
+       </div>
+       <div className='button-row'>
+        <button onClick={this.ball}>Ball</button>
+        <button onClick={this.strike}>Strike</button>
+        <button onClick={this.foul}>Foul</button>
+        <button onClick={this.hit}>Hit</button>
+       </div>
       </div>
     );
   }
--- baseball/src/App.test.js
+++ baseball/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, fireEvent} from 'react-testing-library';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  interface('renders without crashing', () => {
+    const helpers = render(<App />);
+  })
+})
